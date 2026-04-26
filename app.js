469ce1bc8f65f922b3d359b325fd3e0f26232aab
Sofia Lacal <sofialacrodriguez@outlook.com>
2026-04-26 12:23:46 +0200

    .env y otras cositas importantes actualizadas

--- app.js
+++ app.js
@@ -1,12 +1,19 @@
 const express = require("express");
 const cors = require("cors");
-const { sequelize } = require("./models"); //Importar sequelize
+const { sequelize } = require("./models");
 
 const app = express();
 
 app.use(cors({
     origin: 'http://localhost:5173'
 }));
+
+
+//################################ TODO ######################################################################## 
+//CAMBIAR ESTO PARA USAR EL .ENV CON LA RUTA DEL FRONT
+/* app.use(cors({
+    origin: process.env.FRONTEND_URL
+})); */
 
 app.use(express.json());
 
@@ -15,9 +22,18 @@ app.use("/assets", express.static(__dirname + "/assets"));
 
 const PORT = 4004;
 
-//################################ TODO ######################################################################## 
-//Para cuando usemos el .env con toda la configuración de la BD y el puerto :)
-/* const PORT = process.env.PORT || 4004; */
+////Para cuando usemos el .env con toda la configuración de la BD y el puerto :)
+//const PORT = process.env.PORT || 4004;
+
+//########################################## TODO ###########################################################################
+//Poner esto en el package.json para usar el .env con toda la configuración de la BD y el puerto en los scripts, para poder 
+//ejecutar el proyecto con npm start o npm run dev sin tener que poner el env-file cada vez
+// ¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡ Importante, back y front cargan con nom run dev !!!!!!!!!!!!!!!!!!!!!
+/*     "start": "node --env-file=.env app.js",
+    "dev": "node --env-file=.env --watch app.js" */
+
+
+
 
 // -------- IMPORTAR RUTAS --------
 const generalRoutes = require("./routes/general.routes");
